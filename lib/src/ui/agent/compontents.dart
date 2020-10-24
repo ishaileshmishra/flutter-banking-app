f67a5412cf09ec6b68b5fc03988a938a5cedbd2f
shaileshmishra <Yes@383840>
2020-10-25 02:58:44 +0530

    multipart added

--- lib/src/ui/agent/compontents.dart
+++ lib/src/ui/agent/compontents.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 TextStyle buildTextStyle() =>
     TextStyle(fontWeight: FontWeight.bold, fontSize: 16, wordSpacing: 5);
+
+
--- lib/src/ui/agent/kyc_update.dart
+++ lib/src/ui/agent/kyc_update.dart
@@ -1,14 +1,18 @@
 import 'dart:convert';
+import 'dart:io';
 
-import 'package:alok/src/ui/agent/compontents.dart';
+import 'package:alok/src/utils/global_widgets.dart';
+import 'package:async/async.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:http/http.dart' as http;
+import 'package:path/path.dart';
 
 import 'package:alok/res.dart';
+import 'package:alok/src/ui/agent/compontents.dart';
 import 'package:alok/src/ui/user/Components.dart';
-import 'package:alok/src/utils/global_widgets.dart';
 
 class KYCUpdatePage extends StatefulWidget {
   KYCUpdatePage(
@@ -31,7 +35,10 @@ class KYCUpdatePage extends StatefulWidget {
 class _KYCUpdatePageState extends State<KYCUpdatePage> {
   var _amountController = TextEditingController();
   var _remarkController = TextEditingController();
-
+  var multipartFileFrontDocument;
+  var multipartFileBackDocument;
+  String fileNameFront = '';
+  String fileNameBack = '';
   bool _validateAmount = false;
   bool _validateRemark = false;
 
@@ -41,7 +48,7 @@ class _KYCUpdatePageState extends State<KYCUpdatePage> {
     _amountController.text = widget.amount;
   }
 
-  void _validateTextFields() {
+  _validateTextFields(context) {
     setState(() {
       _validateAmount = false;
       _validateRemark = false;
@@ -52,31 +59,111 @@ class _KYCUpdatePageState extends State<KYCUpdatePage> {
     } else if (_remarkController.text.trim().isEmpty) {
       _validateRemark = true;
       return;
+    } else if (multipartFileFrontDocument == null) {
+      showToastWithError(context, 'Please select Adhar card front photo');
+      return;
     }
-    loadAccounts();
+    loadAccounts(context);
   }
 
-  loadAccounts() async {
+  loadAccounts(context) async {
     var box = Hive.box(Res.aHiveDB);
     var agentID = box.get(Res.aUserId);
-
     var credentials = {
       'agentId': agentID,
       'accountNumber': widget.accountNumber,
-      'frontPhoto': 'file',
+      'frontPhoto': multipartFileFrontDocument,
       'backPhoto': 'file',
       'Amount': _amountController.text.trim(),
       'Remark': _remarkController.text.trim(),
     };
-    final response = await http.post(Res.updateKycAPI, body: credentials);
-    if (response.statusCode == 200) {
-      Map userMap = json.decode(response.body);
-      if (userMap['success']) {
-        showToast(context, userMap['message']);
+
+    // final response = await http.post(Res.updateKycAPI, body: credentials);
+    // if (response.statusCode == 200) {
+    //   Map userMap = json.decode(response.body);
+    //   if (userMap['success']) {
+    //     showToast(context, userMap['message']);
+    //   }
+    //   showToastWithError(context, userMap['message']);
+    // } else {
+    //   showToastWithError(context, 'Failed to create KYC');
+    // }
+
+    var postUri = Uri.parse(Res.updateKycAPI);
+    var request = new http.MultipartRequest("POST", postUri);
+    request.fields.addAll(credentials);
+    request.files.add(multipartFileFrontDocument);
+    request.send().then((response) {
+      if (response.statusCode == 200) {
+        response.stream.transform(utf8.decoder).listen((value) {
+          Map userMap = json.decode(value);
+          if (userMap['success']) {
+            showToast(context, userMap['message']);
+            new Future.delayed(const Duration(seconds: 2));
+            Navigator.pop(context);
+          } else {
+            showToastWithError(context, userMap['message']);
+          }
+        });
       }
-      showToastWithError(context, userMap['message']);
-    } else {
-      showToastWithError(context, 'Failed to create KYC');
+    }).catchError((error) {
+      showToastWithError(context, 'FAILED ${error.toString()}');
+    });
+  }
+
+  Future<void> _pickFrontFile() async {
+    FilePickerResult result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      File imageFile = File(result.files.single.path);
+      if (imageFile.existsSync()) {
+        var stream =
+            // ignore: deprecated_member_use
+            http.ByteStream(DelegatingStream.typed(imageFile.openRead()));
+        var length = await imageFile.length();
+        setState(() {
+          imageFile
+              .exists()
+              .then((_) => {
+                    setState(() {
+                      multipartFileFrontDocument = new http.MultipartFile(
+                          'frontPhoto', stream, length,
+                          filename: basename(imageFile.path));
+                      fileNameFront = basename(imageFile.path);
+                    })
+                  })
+              .catchError((onError) {
+            print('Error while loading file: $onError');
+          });
+        });
+      }
+    }
+  }
+
+  Future<void> _pickBackFile() async {
+    FilePickerResult result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      File imageFile = File(result.files.single.path);
+      if (imageFile.existsSync()) {
+        var stream =
+            // ignore: deprecated_member_use
+            http.ByteStream(DelegatingStream.typed(imageFile.openRead()));
+        var length = await imageFile.length();
+        setState(() {
+          imageFile
+              .exists()
+              .then((_) => {
+                    setState(() {
+                      multipartFileBackDocument = new http.MultipartFile(
+                          'backPhoto', stream, length,
+                          filename: basename(imageFile.path));
+                      fileNameBack = basename(imageFile.path);
+                    })
+                  })
+              .catchError((onError) {
+            print('Error while loading file: $onError');
+          });
+        });
+      }
     }
   }
 
@@ -88,31 +175,52 @@ class _KYCUpdatePageState extends State<KYCUpdatePage> {
         elevation: 0,
         backgroundColor: Res.accentColor,
         centerTitle: true,
-        title: Text('KYC Request'),
+        title: Text('KYC Update'),
       ),
       body: Container(
         padding: EdgeInsets.all(30),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 15),
+            SizedBox(height: 25),
             Text(
               'Account Number: ${widget.accountNumber}',
               style: buildTextStyle(),
             ),
 
-            SizedBox(height: 10),
+            SizedBox(height: 20),
             Text(
               '${widget.accountMode}',
               style: buildTextStyle(),
             ),
 
-            SizedBox(height: 10),
+            SizedBox(height: 20),
             Text(
               '${widget.adharCardNumber}',
               style: buildTextStyle(),
             ),
             //=================================
+            SizedBox(height: 20),
+            RaisedButton.icon(
+              onPressed: () {
+                print('Button Clicked.');
+                _pickFrontFile();
+              },
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
+              label: Text(
+                'Adhar card front photo',
+                style: TextStyle(color: Colors.white),
+              ),
+              icon: Icon(
+                Icons.backup,
+                color: Colors.white,
+              ),
+              textColor: Colors.white,
+              splashColor: Colors.red,
+              color: Colors.lightBlue,
+            ),
+
             SizedBox(height: 20),
             TextField(
               keyboardType: TextInputType.number,
@@ -151,7 +259,7 @@ class _KYCUpdatePageState extends State<KYCUpdatePage> {
                 child: Text('SUBMIT'),
                 color: Res.primaryColor,
                 onPressed: () {
-                  _validateTextFields();
+                  _validateTextFields(context);
                 },
               ),
             ),
