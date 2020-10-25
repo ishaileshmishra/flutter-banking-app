c75d8a51917b6facba16f2547d4d72f0b8d38c7b
shaileshmishra <Yes@383840>
2020-10-25 14:32:22 +0530

    login UI fixed

--- lib/src/ui/login/LoginPage.dart
+++ lib/src/ui/login/LoginPage.dart
@@ -190,7 +190,7 @@ class _LoginPageState extends State<LoginPage> {
                   child: showWelcomeText(),
                 ),
               ),
-              Flexible(
+              Expanded(
                 child: Container(
                   decoration: BoxDecoration(
                     color: Colors.white,
@@ -199,22 +199,35 @@ class _LoginPageState extends State<LoginPage> {
                       topRight: Radius.circular(30),
                     ),
                   ),
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 8),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        //=======================
-                        SizedBox(height: 30),
-                        _textFieldMobile(),
-                        //=======================
-                        SizedBox(height: 10),
-                        _textFieldPassword(),
-                        //=======================
-                        SizedBox(height: 30),
-                        _loginButton()
-                        //=======================
-                      ],
+                  child: SingleChildScrollView(
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30, vertical: 8),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 20),
+                          Text("Sign In",
+                              style: TextStyle(
+                                fontSize: 22,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.left),
+
+                          //=======================
+                          SizedBox(height: 30),
+                          _textFieldMobile(),
+                          //=======================
+                          SizedBox(height: 10),
+                          _textFieldPassword(),
+                          //=======================
+                          SizedBox(height: 30),
+                          _loginButton()
+                          //=======================
+                        ],
+                      ),
                     ),
                   ),
                 ),
